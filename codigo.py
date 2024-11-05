9c9ccd872e259eb7cc3c91c3ad3c8b000afde438
ElCherry69 <thumasterkill2300@gmail.com>
2024-11-05 17:20:38 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,4 +1,12 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-FUSHIBOOL.PNG
+
+st.sidebar.title('Mi primera barra lateral de Streamlit')
+st.sidebar.header('¡Hola, Barra Lateral!')
+st.sidebar.write('Esto es una barra lateral')
+st.sidebar.image('FUSHIBOOL.PNG')
+if st.sidebar.button('Haz clic pero en la barra lateral'):
+    st.sidebar.write('¡Has hecho clic en el boton de la barra lateral!')
+user_input = st.sidebar.text_input('Escribe algo en la barra:')
+st.sidebar.write('Escribe en la barra:', user_input)
